671b4dc6725dca3de453e84c20c741e4610b06f9
Rudy Ravelin <rudy@noukod.com>
2018-12-13 16:29:19 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,5 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-
-  def change
+def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
